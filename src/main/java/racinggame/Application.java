4bca0f2c8e834823ea169dfa9e06c54bba1d5e14
YONGJU <odetsuo@gmail.com>
2021-10-12 16:12:11 +0900

    refactor : 실행결과 메시지

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -8,7 +8,7 @@ import racinggame.ui.ResultView;
 
 public class Application {
 
-    private static final String RESULT_MESSAGE = "실행 결과";
+    private static final String RESULT_MESSAGE = "%n실행 결과%n";
     private static final String ERROR = "[ERROR] 잘못 입력하셨습니다. 다시 입력해주세요. ";
 
     public static void main(String[] args) {
@@ -16,7 +16,7 @@ public class Application {
         RacingCar racingCar = inputNames();
         int tryCount = InputView.tryCount();
 
-        System.out.println(RESULT_MESSAGE);
+        System.out.printf(RESULT_MESSAGE);
         for (int i = 0; i < tryCount; i++) {
             racingCar.move();
             ResultView.printRace(racingCar.cars());
